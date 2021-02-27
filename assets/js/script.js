4aee9180f53cd23dde64fdbd76ff131d4470da21
rjhelm <77085453+rjhelm@users.noreply.github.com>
2021-02-27 16:18:56 -0700

    testing layout and design of page for responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,100 @@
 // Variables used by moment.js
-let now = moment().format("H A");
-let today = moment().format("MMMM Do YYYY, h:mm:ss a");
-
-// array for the workday times
-let workday = [
-  { time: "9am", event: "" },
-  { time: "10am", event: "" },
-  { time: "11am", event: "" },
-  { time: "12pm", event: "" },
-  { time: "1pm", event: "" },
-  { time: "2pm", event: "" },
-  { time: "3pm", event: "" },
-  { time: "4pm", event: "" },
-  { time: "5pm", event: "" },
-];
+//let now = moment().format("H A");
+//let today = moment().format("MMMM Do YYYY, h:mm:ss a");
 
 // Current Day Variable
-$("#currentDay").text(today);
+//$("#currentDay").text(today);
+
+// array for the workday times
+//let workday = [
+ // { time: "9am", event: "" },
+ // { time: "10am", event: "" },
+  //{ time: "11am", event: "" },
+ // { time: "12pm", event: "" },
+ // { time: "1pm", event: "" },
+ // { time: "2pm", event: "" },
+ //{ time: "3pm", event: "" },
+ //{ time: "4pm", event: "" },
+  //{ time: "5pm", event: "" },
+//];
 
 // Check local storage
-let daysEvents = JSON.parse(localStorage.getItem("workToday"));
-if (daysEvents) {
-  workday = daysEvents;
-}
+//let daysEvents = JSON.parse(localStorage.getItem("workToday"));
+//if (daysEvents) {
+  //workday = daysEvents;
+//}
 
 // creates the rows in html and gives layout once on the page
-workday.forEach(function (timeBlock, index) {
-  let timeDefine = timeBlock.time;
-  let timeColor = rowColors(timeDefine);
-  let row =
-    '<div class="time-block" id="' +
-    index +
-    '"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
-    timeDefine +
-    '</div><textarea class="form-control ' +
-    timeColor +
-    '">' +
-    timeBlock.event +
-    '</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
-  $(".container").append(row);
-});
+//workday.forEach(function (timeBlock, index) {
+  //let timeDefine = timeBlock.time;
+ // let timeColor = rowColors(timeDefine);
+ // let row =
+ //   '<div class="time-block" id="' +
+  //  index +
+  //  '"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
+  //  timeDefine +
+  //  '</div><textarea class="form-control ' +
+ //   timeColor +
+  //  '">' +
+  //  timeBlock.event +
+  //  '</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
+ // $(".container").append(row);
+//});
 
 // add colors to the rows we created based on the time
-function rowColors(time) {
-  let currentEvent = moment(now, "H A");
-  let userEvent = moment(time, "H A");
-  if (currentEvent.isBefore(userEvent) === true) {
-    return "future";
-  } else if (currentEvent.isAfter(userEvent) === true) {
-    return "past";
-  } else {
-    return "present";
-  }
-}
+//function rowColors(time) {
+ // let currentEvent = moment(now, "H A");
+ // let userEvent = moment(time, "H A");
+ // if (currentEvent.isBefore(userEvent) === true) {
+   // return "future";
+ // } else if (currentEvent.isAfter(userEvent) === true) {
+   // return "past";
+ // } else {
+  //  return "present";
+ // }
+//}
 // Save the event entered by user
-$(".saveBtn").on("click", function () {
-  let blockID = parseInt($(this).closest(".time-block").attr("id"));
-  let userTask = $.trim($(this).parent().siblings("textarea").val());
-  workday[blockID].event = userTask;
-  localStorage.setItem("workToday", JSON.stringify(workday));
-});
+//$(".saveBtn").on("click", function () {
+  //let blockID = parseInt($(this).closest(".time-block").attr("id"));
+  //let userTask = $.trim($(this).parent().siblings("textarea").val());
+ // workday[blockID].event = userTask;
+  //localStorage.setItem("workToday", JSON.stringify(workday));
+//});
 
+$(document).ready(function () {
+  let setTime = moment().hour();
+  let keepTime = function () {
+      $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm A'));
+  };
+  setTime();
+  setInterval(update, 1000);
+
+  let workHours = [
+    "9:00 AM",
+    "10:00 AM",
+    "11:00 AM",
+    "12:00 PM",
+    "1:00 PM",
+    "2:00 PM",
+    "3:00 PM",
+    "4:00 PM",
+    "5:00 PM",
+    ];
+
+  for (var i = 0; i < workHours.length; i++) {
+      let useWorkHour = [i + 9];
+      let timeRow = $('<tr class="rowColor">');
+      let hourRow = $('<td class="align-middle"><h3 class="time" id="${hours[i]}" use-work-hour="${useWorkHour}">${hours[i]}</h3><td>');
+      let task = $('<td class="align-middle"><textarea class="form-control taskText" id="${useWorkHour}text" rows="3"></textarea></td>');
+      let save = $('<td class="align-middle"><i class="far fa-save fa-3x saveBtn" use-work-hour="${useWorkHour}"></i></td>');
+      timeRow.append(hourRow, task, save);
+      $('tbody').append(timeRow);  
+    }
+    init();
+    function init() {
+        for (let k = 9; k < 18; k++) {
+            $('#' + k + 'text').val(localStorage.getItem(k));
+        }
+    }
+    
+});

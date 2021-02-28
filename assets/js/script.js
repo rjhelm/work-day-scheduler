c5187481efe02243b938e6aaed7b25169506f70e
rjhelm <77085453+rjhelm@users.noreply.github.com>
2021-02-27 17:42:32 -0700

    changes to make display more responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 // Workday Scheduler Code
 $(function () {});
 // Variables used by moment.js
-let now = moment().format("HH:MM A");
+let now = moment().format("HH:MM");
 let today = moment().format("MMMM Do YYYY, hh:mm A");
 
 // Current Day Variable
 $("#currentDay").text(today);
 
-// array for the workday times
-let workday = [
+// array for the workHours times
+let workHours = [
     { time: "9:00 AM", event: "" },
     { time: "10:00 AM", event: "" },
     { time: "11:00 AM", event: "" },
@@ -23,23 +23,23 @@ let workday = [
 // Check local storage
 let daysEvents = JSON.parse(localStorage.getItem("workToday"));
     if (daysEvents) {
-    workday = daysEvents;
-    }
+    workHours = daysEvents;
+}
 
 // creates the rows in html and gives layout once on the page
-workday.forEach(function (timeBlock, index) {
-    let timeDefine = timeBlock.time;
-    let timeColor = rowColors(timeDefine);
-    let row ='<div class="time-block" id="' +
+workHours.forEach(function (timeDisplay, index) {
+    let taskTime = timeDisplay.time;
+    let timeColor = rowColors(taskTime);
+    let scheduleRow ='<div class="time-display" id="' +
     index +
-    '"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
-    timeDefine +
+    '"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-center pr-3 pt-3">' +
+    taskTime +
     '</div><textarea class="form-control ' +
     timeColor +
     '">' +
-    timeBlock.event +
+    timeDisplay.event +
     '</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
-    $(".container").append(row);
+    $(".container").append(scheduleRow);
 });
 
 // add colors to the rows we created based on the time
@@ -54,12 +54,12 @@ function rowColors(time) {
     return "present";
   }
 }
+
 // Save the event entered by user
 $(".saveBtn").on("click", function () {
-  let blockID = parseInt($(this).closest(".time-block").attr("id"));
+  let displayID = parseInt($(this).closest(".time-display").attr("id"));
   let userTask = $.trim($(this).parent().siblings("textarea").val());
-  workday[blockID].event = userTask;
-  localStorage.setItem("workToday", JSON.stringify(workday));
+  workHours[displayID].event = userTask;
+  localStorage.setItem("workToday", JSON.stringify(workHours));
 });
-
 

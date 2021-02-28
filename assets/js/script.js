6083340d5d243f433bf75031efe6982b4c54aaa4
rjhelm <77085453+rjhelm@users.noreply.github.com>
2021-02-28 14:52:14 -0700

    final cleanup of code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 // Workday Scheduler Code
 $(function () {});
-
+// moment time variables
 let now = moment().format("H A");
 
 let today = moment().format("MMMM Do YYYY, h:mm A");
 
 $("#currentDay").text(today);
 
+// array for the typical work hours 
 let workHours = [
     { time: "9 AM", event: "" },
     { time: "10 AM", event: "" },
@@ -23,12 +24,13 @@ let workHours = [
 workHours.forEach(function (timeBlock, index) {
     let taskTime = timeBlock.time;
     let timeColor = rowColors(taskTime);
+    // creates the rows for each time in the array along with style 
     let scheduleRow ='<div class="time-block" id="' +
     index + '"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-center pr-3 pt-3">' +
     taskTime + '</div><textarea class="form-control ' +
     timeColor + '">' +
     timeBlock.event + '</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
-
+    // places our rows within the html document in the container that is already in html
     $(".container").append(scheduleRow);
 });
 
@@ -36,6 +38,7 @@ workHours.forEach(function (timeBlock, index) {
 function rowColors(time) {
     let currentEvent = moment(now, "H A");
     let userEvent = moment(time, "H A");
+    // color changes depending on time and if an event has passed--tells user status of the entered task
     if (currentEvent.isBefore(userEvent) === true) {
      return "future";
     } else if (currentEvent.isAfter(userEvent) === true) {
@@ -45,7 +48,7 @@ function rowColors(time) {
   }
 }
 
-// Check local storage
+// local storage check 
 let daysEvents = JSON.parse(localStorage.getItem("workToday"));
     if (daysEvents) {
     workHours = daysEvents;
